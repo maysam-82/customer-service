e6931cb63b219ee75d9d424b17a27b18da44bdd0
maysam <maysam82.dev@gmail.com>
2020-09-19 03:31:30 +1000

    remove baseUrl in apis since moxios has issue with baseUrl and is not stub request properly - add baseURL to proxy instead in package.json

--- client/src/services/customers/apis.ts
+++ client/src/services/customers/apis.ts
@@ -1,25 +1,21 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001',
-});
-
 export async function getData<T>(url: string): Promise<T> {
-    const response = await axiosInstance.get<T>(url);
+    const response = await axios.get<T>(url);
     return response.data;
 }
 
 export async function deleteData(url: string) {
-    const response = await axiosInstance.delete(url);
+    const response = await axios.delete(url);
     return response.data;
 }
 
 export async function postData<T>(url: string, data: T): Promise<T> {
-    const response = await axiosInstance.post<T>(url, data);
+    const response = await axios.post<T>(url, data);
     return response.data;
 }
 
 export async function editData<T>(url: string, data: T): Promise<T> {
-    const response = await axiosInstance.put<T>(url, data);
+    const response = await axios.put<T>(url, data);
     return response.data;
 }
