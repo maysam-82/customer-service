e946912ce5f50d70ad8179b3df9303e0efc57f3f
maysam <maysam82.dev@gmail.com>
2020-09-18 12:36:50 +1000

    Create actions for toast message

--- client/src/redux/actions/actionTypes.ts
+++ client/src/redux/actions/actionTypes.ts
@@ -3,14 +3,19 @@ import {
     IGetCustomersStart,
     IGetCustomersSuccess,
 } from './customers/customers.types';
+import { IAddToast, IRemoveToast } from './toasts/toasts.types';
 
 export enum ActionTypes {
     GET_CUSTOMERS_SUCCESS,
     GET_CUSTOMERS_START,
     GET_CUSTOMERS_FAIL,
+    ADD_TOAST,
+    REMOVE_TOAST,
 }
 
 export type CustomersActions =
     | IGetCustomersSuccess
     | IGetCustomersStart
     | IGetCustomersFail;
+
+export type ToastActions = IAddToast | IRemoveToast;
---
+++ client/src/types/toast.d.ts
@@ -0,0 +1,5 @@
+export interface IToast {
+    toastMessage: string;
+    toastType: string;
+    toastId: string;
+}
