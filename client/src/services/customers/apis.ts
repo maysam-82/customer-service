df93b13fefde93e87bee7a53bf946b2d47ba9552
maysam <maysam82.dev@gmail.com>
2020-09-18 14:59:16 +1000

    Add delete method to axios instance to delete data

--- client/src/services/customers/apis.ts
+++ client/src/services/customers/apis.ts
@@ -8,3 +8,8 @@ export async function getData<T>(url: string): Promise<T> {
     const response = await axiosInstance.get(url);
     return response.data;
 }
+
+export async function deleteData(url: string) {
+    const response = await axiosInstance.delete(url);
+    return response.data;
+}
