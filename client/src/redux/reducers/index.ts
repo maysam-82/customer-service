3c2a442081f7be3806efee21664ae29c3e905fa7
maysam <maysam82.dev@gmail.com>
2020-09-18 12:43:12 +1000

    Create Reducer for toast message

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,12 +1,16 @@
 import { combineReducers } from 'redux';
+import { IToast } from '../../types/toast';
 import customersReducer, { ICustomersState } from './customers/customers';
+import toastsReducer from './toasts/toasts';
 
 export interface IStoreState {
     customers: ICustomersState;
+    toasts: IToast[];
 }
 
 const reducers = combineReducers<IStoreState>({
     customers: customersReducer,
+    toasts: toastsReducer,
 });
 
 export default reducers;
